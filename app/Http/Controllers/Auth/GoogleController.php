d7c9255a26746974e25d6c681ae47ed22307157c
BiUsagi <tubanovel@gmail.com>
2025-06-05 20:23:29 +0700

    install gglogin

---
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try {
+            $googleUser = Socialite::driver('google')->stateless()->user();
+
+            $user = User::where('email', $googleUser->getEmail())->first();
+
+            if (!$user) {
+                $user = User::create([
+                    'name' => $googleUser->getName(),
+                    'email' => $googleUser->getEmail(),
+                    'email_verified_at' => now(),
+                    'password' => bcrypt(Str::random(24)),
+                ]);
+            }
+
+            Auth::login($user, true);
+
+            return redirect()->intended('/dashboard');
+        } catch (\Exception $e) {
+            return redirect('/login')->withErrors('Đăng nhập Google thất bại!');
+        }
+    }
+}
