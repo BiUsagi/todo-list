717b46555da39157783bc3e21edc52aff865db98
BiUsagi <tubanovel@gmail.com>
2025-06-25 18:09:32 +0700

    add api route

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\TodoController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Todo;
+use App\Http\Controllers\Api\TodoController as ApiTodoController;
+
+Route::middleware('auth:sanctum')->apiResource('todos', ApiTodoController::class);
